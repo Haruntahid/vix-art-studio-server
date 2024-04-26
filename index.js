95ad4c614bdc053fe670dd72347e87047c8d0d53
haruntahid <tahidtaha997@gmail.com>
2024-04-26 22:15:51 +0600

    add craft data store on database

--- index.js
+++ index.js
@@ -30,6 +30,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // create database and collection on mongoDB
+    const craftsCollection = client.db("crafts").collection("crafts");
+
+    // add craft
+    app.post("/crafts", async (req, res) => {
+      const newCraft = req.body;
+      const result = await craftsCollection.insertOne(newCraft);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
