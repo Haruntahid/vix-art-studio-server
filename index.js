509a7d34e4054c0f48f2eb67ac3cf923e51c360e
haruntahid <tahidtaha997@gmail.com>
2024-04-27 22:39:38 +0600

    get specific craft data from database based on email

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
@@ -44,6 +44,23 @@ async function run() {
     // get all craft data from database(Read)
     app.get("/crafts", async (req, res) => {
       const cursor = craftsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // get a specific craft data from database based on id(Read)
+    app.get("/craft/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await craftsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // get a specific craft data from database based on email(Read)
+    app.get("/crafts/:email", async (req, res) => {
+      const { email } = req.params;
+      const query = { email: email };
+      const cursor = craftsCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
